550b3b807b38c5f30373e2f7685dc3131f98d328
Puneethhs099 <115694645+Puneethhs099@users.noreply.github.com>
2023-04-18 10:16:41 +0530

    Update unittesting.js
    unittesting

--- unittesting.js
+++ unittesting.js
@@ -44,3 +44,53 @@ function main()
 
 main()
 
+
+
+class fraction
+{
+    constructor( num , den )
+    {
+        this.num = num;
+        this.den = den;
+    }
+    subtract(x)
+    {
+        var subtract = subtract_fraction(this,x);
+        console.log("%d/%d",subtract.num,subtract.den);
+    }
+
+}
+
+function gcd(a,b)
+{
+    var gcd=1;
+    for(let i=2;(i<=a&&i<=b);i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    return gcd;
+}
+
+function subtract_fraction(x,y)
+{
+    var hcf=gcd(x.den,y.den);
+    var result = new fraction;
+    result.den = x.den*y.den / hcf;
+    result.num = x.num*(result.den/x.den) - y.num*(result.den/y.den);
+    return result;
+}
+
+function main()
+{
+    var x = new fraction(1,2);
+    var y = new fraction(1,2);
+    (x.subtract(y));
+}
+
+main()
+
+
+
